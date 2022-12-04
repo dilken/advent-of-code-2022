f3fd351d585afeba30334d877490f9ff3a025f4f
Dilken Dilber <dilken.dilber@naktergal.tech>
2022-12-04 14:42:44 +0100

    completed day 4

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,5 @@ fn main() {
     day01::print_elf_carrying_most_kcal();
     day02::print_rock_paper_scissors_scores();
     day03::print_elf_backpack_items();
-   // day04::get_t(); 
+    day04::print_elf_sections(); 
 }
